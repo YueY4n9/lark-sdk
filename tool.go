9ecaaa23134853d5fb8d0a9845eaa92a2d3214be
yuey4n9 <42286278+YueY4n9@users.noreply.github.com>
2024-12-26 16:21:56 +0800

    Update tool.go

--- tool.go
+++ tool.go
@@ -10,3 +10,15 @@ func ParseForm(formStr string) ([]FormWidget, error) {
 	}
 	return res, nil
 }
+
+func ParseForm2Map(formStr string) (map[string]FormWidget, error) {
+	widgets, err := ParseForm(formStr)
+	if err != nil {
+		return nil, err
+	}
+	res := make(map[string]FormWidget)
+	for _, widget := range widgets {
+		res[widget.Name] = widget
+	}
+	return res, nil
+}
